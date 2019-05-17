d06ea4e8bdd3412342834b2ebd8acbfb14d39684
kishore <kishorekumarnetala@gmail.com>
2019-05-17 08:09:42 +0530

    new changes

--- app.js
+++ app.js
@@ -5,17 +5,28 @@ const forecast = require('./utils/weather')
 const buffer = fs.readFileSync("creds.json")
 const creds = JSON.parse(buffer.toString())
 
-
-geocode(creds, "India", (error, data)=>{
-    if (error){
-        return console.log("Error:", error)
-    }
-
-    forecast(creds, {latitude: data.latitude, longitude: data.longitude}, (error, forecaset_data)=>{
-        if(error){
-            return console.log(error)
+const address = process.argv[2]
+if (!address){
+    console.log("Please provide a location")
+}
+else{
+    geocode(creds, "India", (error, data)=>{
+        if (error){
+            return console.log("Error:", error)
         }
-        console.log(data.location)
-        console.log(forecaset_data)
+        
+        forecast(creds, {latitude: data.latitude, longitude: data.longitude}, (error, forecaset_data)=>{
+            if(error){
+                return console.log(error)
+            }
+            console.log(data.location)
+            console.log(forecaset_data)
+        })
     })
+}
+
+// const url = "https://api.darksky.net/forecast/"+creds.weather_api_key+"/37.8267,-122.4233"
+forecast(creds, {latitude: 37.8267, longitude: -122.4233}, (error, data)=>{
+    console.log(error)
+    console.log(data)
 })
